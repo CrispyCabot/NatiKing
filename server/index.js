c3d8598da34c8d866c1e6b5da8d71f95b8e788cd
Chris Bridewell <cjbaseball5@gmail.com>
2022-02-04 10:11:59 -0500

    test

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ mongoose.connect(
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-    console.log(proess.env.MONGO_ATLAS_DB);
+    console.log(process.env.MONGO_ATLAS_DB);
 });
 app.use(express.static("server_html"));
 app.use(function(req, res, next) {
