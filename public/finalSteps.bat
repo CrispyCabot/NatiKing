0b7aac935d9e84b425272ce95963063e762d42a1
Chris Bridewell <cjbaseball5@gmail.com>
2022-02-04 15:48:32 -0500

    Front end batch script made

--- public/finalSteps.bat
+++ public/finalSteps.bat
@@ -1,8 +1,6 @@
 cd "C:\Users\cjbas\Desktop\jamenwalz.github.io"
-rmdir css /s /q
-rmdir img /s /q
-rmdir js /s /q
-DEL 404.html
-DEL index.html
-DEL favicon.ico
+copy 404.html index.html
+git add *
+git commit -m "See CrispyCabot/NatiKing"
+git push
 cd "C:\Users\cjbas\Desktop\NatiKing\public"
