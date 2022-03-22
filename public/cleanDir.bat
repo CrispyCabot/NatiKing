ab39d640b31361cf78cb444bb18ebf5ada13626f
Chris Bridewell <cjbaseball5@gmail.com>
2022-03-22 17:15:50 -0400

    modified metamaker and may have added analytics

--- public/cleanDir.bat
+++ public/cleanDir.bat
@@ -1,7 +1,4 @@
 cd "C:\Users\cjbas\Desktop\jamenwalz.github.io"
-rmdir css /s /q
-rmdir img /s /q
-rmdir js /s /q
 DEL 404.html
 DEL index.html
 DEL favicon.ico
--- public/vue.config.js
+++ public/vue.config.js
@@ -14,4 +14,20 @@ module.exports = {
     outputDir: path.resolve(__dirname, "../../jamenwalz.github.io"),
     indexPath: "404.html",
     transpileDependencies: ["vue-meta"],
+    chainWebpack: (config) => {
+        if (config.plugins.has("extract-css")) {
+            const extractCSSPlugin = config.plugin("extract-css");
+            extractCSSPlugin &&
+                extractCSSPlugin.tap(() => [{
+                    filename: "[name].css",
+                    chunkFilename: "[name].css",
+                }, ]);
+        }
+    },
+    configureWebpack: {
+        output: {
+            filename: "[name].js",
+            chunkFilename: "[name].js",
+        },
+    },
 };
