0d62427e77235e68c596aa38e7010f3b9afbcefb
Chris Bridewell <cjbaseball5@gmail.com>
2022-04-01 15:08:46 -0400

    new twitter card

--- metaMaker.py
+++ metaMaker.py
@@ -23,7 +23,7 @@ html = '''
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:url" content="URL_PLACEHOLDER" />
     <meta name="twitter:title" content="TITLE_PLACEHOLDER" />
     <meta name="twitter:description" content="DESCRIPTION_PLACEHOLDER" />
